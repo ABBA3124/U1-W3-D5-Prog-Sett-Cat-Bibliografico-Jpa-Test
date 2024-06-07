d9dcca550579ccc3293bfc45d7468074af1e33cd
ABBA3124 <davideabbadessa73@gmail.com>
2024-06-07 15:08:46 +0200

    rimuovi elemento tramite ISBN
    Rimozione di un elemento del catalogo dato un codice ISBN

--- src/main/java/org/davideabbadessa/Application.java
+++ src/main/java/org/davideabbadessa/Application.java
@@ -4,9 +4,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import org.davideabbadessa.dao.ElementoCatalogoDAO;
-import org.davideabbadessa.entities.Libro;
-import org.davideabbadessa.entities.Periodicita;
-import org.davideabbadessa.entities.Rivista;
 
 public class Application {
     public static void main(String[] args) {
@@ -16,26 +13,36 @@ public class Application {
 
         ElementoCatalogoDAO dao = new ElementoCatalogoDAO(em);
 
-        //aggiugno un libro
-        Libro libro = new Libro();
-        libro.setCodiceISBN("65411");
-        libro.setTitolo("Java Programming");
-        libro.setAnnoPubblicazione(2025);
-        libro.setNumeroPagine(20);
-        libro.setAutore("i");
-        libro.setGenere("Programming");
-        dao.aggiungiElemento(libro);
-        System.out.println(libro + " okok libro ");
+        /*--------------------------------------------Aggiunta di un elemento del catalogo-------------------------------------------*/
 
-        //aggiungo una rivista
-        Rivista rivista = new Rivista();
-        rivista.setCodiceISBN("61564156411");
-        rivista.setTitolo("nuova rivista 5");
-        rivista.setAnnoPubblicazione(2031);
-        rivista.setNumeroPagine(36);
-        rivista.setPeriodicita(Periodicita.MENSILE);
-        dao.aggiungiElemento(rivista);
-        System.out.println(rivista + " okok rivista ");
+//        //aggiugno un libro
+//        Libro libro = new Libro();
+//        libro.setCodiceISBN("65411");
+//        libro.setTitolo("Java Programming");
+//        libro.setAnnoPubblicazione(2025);
+//        libro.setNumeroPagine(20);
+//        libro.setAutore("i");
+//        libro.setGenere("Programming");
+//        dao.aggiungiElemento(libro);
+//        System.out.println(libro + " okok libro ");
+//
+//        //aggiungo una rivista
+//        Rivista rivista = new Rivista();
+//        rivista.setCodiceISBN("61564156411");
+//        rivista.setTitolo("nuova rivista 5");
+//        rivista.setAnnoPubblicazione(2031);
+//        rivista.setNumeroPagine(36);
+//        rivista.setPeriodicita(Periodicita.MENSILE);
+//        dao.aggiungiElemento(rivista);
+//        System.out.println(rivista + " okok rivista ");
+
+
+        /*-------------------------------------------Rimozione di un elemento del catalogo dato un codice ISBN-------------------------------------------*/
+
+        //rimuovo un elementocatalogo tramite ISBN
+        dao.rimuoviElemento("16516514");
+
+        /*--------------------------------------------Ricerca per ISBN-------------------------------------------*/
 
     }
 }
--- src/main/java/org/davideabbadessa/dao/ElementoCatalogoDAO.java
+++ src/main/java/org/davideabbadessa/dao/ElementoCatalogoDAO.java
@@ -1,6 +1,7 @@
 package org.davideabbadessa.dao;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import org.davideabbadessa.entities.ElementoCatalogo;
 
 public class ElementoCatalogoDAO {
@@ -16,5 +17,26 @@ public class ElementoCatalogoDAO {
         em.getTransaction().commit();
     }
 
-    
+
+    public void rimuoviElemento(String codiceISBN) {
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
+            ElementoCatalogo elemento = em.createQuery("SELECT e FROM ElementoCatalogo e WHERE e.codiceISBN = :codiceISBN", ElementoCatalogo.class)
+                    .setParameter("codiceISBN", codiceISBN)
+                    .getSingleResult();
+            if (elemento != null) {
+                em.remove(elemento);
+                System.out.println("Elemento con codice ISBN " + codiceISBN + " rimosso.");
+            } else {
+                System.out.println("Elemento con codice ISBN " + codiceISBN + " non trovato.");
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
 }
