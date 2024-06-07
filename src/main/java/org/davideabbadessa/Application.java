5b15a39751caada58e00aead6cb32d281d6d290a
ABBA3124 <davideabbadessa73@gmail.com>
2024-06-07 11:00:09 +0200

    start

--- src/main/java/org/davideabbadessa/Application.java
+++ src/main/java/org/davideabbadessa/Application.java
@@ -1,8 +1,9 @@
 package org.davideabbadessa;
 
 public class Application {
+//    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("U1-W3-D5-Prog-Sett-Cat-Bibliografico-Jpa-Test");
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Ciao funziono!");
     }
 }
